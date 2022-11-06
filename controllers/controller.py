51b3dea3f2522cedfe072089f0e594db340b6eec
Gintare Dhavde <omkardhawde@MacBook-Air.local>
2022-11-06 12:09:15 +0000

    mvp done

--- controllers/controller.py
+++ controllers/controller.py
@@ -1,8 +1,8 @@
-from flask import render_template
+from flask import render_template, request, redirect
 
 from app import app
 from models.book import Book
-from models.book_list import book_list
+from models.book_list import book_list, add_new_book, delete_book
 
 @app.route('/book')
 def index():
@@ -10,8 +10,31 @@ def index():
 
 
 @app.route('/book/<index>')
-def children_book():
-  children_book = book1
+def book(index):
+  return render_template('book1.html', book = book_list[int(index)])
   
   return render_template('order.html', book=children_book)
+
+@app.route('/book', methods = ['POST'])
+def add_book():
+    title = request.form["title"]
+    author = request.form["author"]
+    genre = request.form["genre"]
+    new_book = Book(title, author, genre)
+    add_new_book(new_book)
+
+    return  redirect('/books') 
+    # return render_template('index.html', title='Library', book=book)
+@app.route('/events/delete/<title>', methods=['POST'])
+def delete(title):
+  delete_book(title)
+  return redirect('/events')
+   
+
+
+ 
+
+
+
+
 
--- models/book_list.py
+++ models/book_list.py
@@ -6,5 +6,14 @@ book3 = Book("Naruto vol.1", "M. Kishimoto", "Adventure/ Fantasy/ Comedy/ Martia
 
 book_list = [book1, book2, book3]
 
-def chosen_book(book):
-    book = book1
+def add_new_book(book):
+    book_list.append(book)
+
+def delete_book(book_title):
+    book_to_delete = None
+    for book in book_list:
+        if book.title == book_title:
+            book_to_delete = book
+            break
+
+    book_list.remove(book_to_delete)
